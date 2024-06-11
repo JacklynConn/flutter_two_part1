6b9a9b96883626b56e4333f4a45c65f838415062
mak_mach <makmach1122@gmail.com>
2024-06-11 11:25:24 +0700

    2.9. Inheritance and Polymorphism

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,22 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_two_part1/my_login.dart';
 
+class Vehicle{
+   String name = '';
+   void drive(){
+     print("Vehicle  $name is driving");
+   }
+}
+
+class Car extends Vehicle{
+  @override
+  void drive() {
+    name = "Motor Car";
+    super.drive();
+    print("Car is driving");
+  }
+}
+
 void main() {
   runApp(const MyApp());
 }
@@ -11,9 +27,13 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
 
-    MyLogin login = MyLogin();
-    login.password = 'abc123'; // setter = write only
-    print(login.hashPassword); // getter = read only
+    Car car = Car();
+    car.drive();
+
+
+    // Polymorphism
+    Vehicle v1 = Car();
+    v1.drive();
 
     return MaterialApp(
       title: 'Flutter Demo',
