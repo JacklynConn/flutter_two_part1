f6e1f1cbc55aaf657dc9528faa1c5c39c068f8fa
mak_mach <makmach1122@gmail.com>
2024-06-11 09:30:03 +0700

    2.6. Class and Object

--- lib/main.dart
+++ lib/main.dart
@@ -1,40 +1,25 @@
 import 'package:flutter/material.dart';
 
 class Student {
-  late int id;
-  late String name;
-
-  // Student(int id, String name) {
-  //   this.id = id;
-  //   this.name = name;
-  //
-
-  Student({required this.id, required this.name});
-
-  Student.fromDB(String db) {
-    this.id = 10;
-    this.name = "John";
-  }
-
   void show() => print('Student');
   void _display() => print("Student");
 }
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
-    Student s1 =
-        Student(id: 10, name: "John"); // instantiate + call to constructor
-    Student s2 = Student.fromDB("DB"); // instantiate + call to constructor
-    print(s2.name);
-    print(s1.id);
-    print(s1.name);
+
+
+    Student s1 = Student();
     s1.show();
-    s1._display(); // private method can be accessed outside the class
+
+    s1._display();
 
     return MaterialApp(
       title: 'Flutter Demo',
@@ -42,57 +27,19 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-              style: TextStyle(fontSize: 25),
-              textAlign: TextAlign.center,
-            ),
-            Text(
-              '$_counter',
-              style: TextStyle(fontSize: 30),
-            ),
-          ],
+      home: Container(
+        color: Colors.white,
+        child: Center(
+          child: ElevatedButton(
+            onPressed: () {
+              Student student = Student();
+              student.show();
+              student._display();
+            },
+            child: const Text('Click Me'),
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        backgroundColor: Colors.deepPurple,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
---
+++ lib/sample.dart
@@ -0,0 +1,9 @@
+import 'package:flutter_two_part1/main.dart';
+
+class Customer {
+  void hello() {
+    Student s1; // create object s1
+    new Student(); // instantiate = create instance
+    s1 = new Student(); // instantiate + point s1 to instance
+  }
+}
